6b0428798bccb265a9ead96361a66679de9033e5
Elliott Coleman <1705665@brunel.ac.uk>
2018-12-17 22:07:16 +0000

    change file structure and coup[le things

--- src/App.js
+++ src/App.js
@@ -34,24 +34,13 @@ class App extends Component {
     this.removeTimers = this.removeTimers.bind(this)
   }
 
-
-
   moveFood() {
-    // if (this.moveFoodTimeout) clearTimeout(this.moveFoodTimeout)
     const x = parseInt(Math.random() * this.numCells);
     const y = parseInt(Math.random() * this.numCells);
     this.setState({ food: [x, y] });
-    // this.moveFoodTimeout = setTimeout(this.moveFood, 5000)
-
-
   }
 
   moveSnake() {
-    // if (this.state.computer !== this.state.currentGameplayComputer) {
-    //   this.pauseGame()
-    //   return
-    // }
-
     this.setState({changingDirection: false})
 
 
@@ -61,7 +50,6 @@ class App extends Component {
     snake.unshift(head);
 
     if(this.checkIfAteFood()) {
-      //this.incrementScore(this.state.score + 10)
       this.setState({score: this.state.score + 10})
       this.socket.emit('updateScore', this.state.score)
 
@@ -74,12 +62,6 @@ class App extends Component {
     !this.isValid(head, snake) && this.endGame()
   }
 
-  // incrementScore(score) {
-    
-
-  //   this.socket.emit('updateScore', this.state.score)
-  // }
-
   checkIfAteFood() {
     return (this.state.snake[0].x === this.state.food[0] && this.state.snake[0].y === this.state.food[1])
   }
@@ -87,38 +69,33 @@ class App extends Component {
   isValid(head, snake) {
     for (let i = 4; i < snake.length; i++) {
       if (snake[i].x === head.x && snake[i].y === head.y) {
-        console.log("OVERLap")
         return false
       }
     }
 
     if (head.x === -1) {
-      console.log("left wall hit")
-
       let data = {yaxis: this.state.snake[0].y, snakeLength: this.state.snake.length }
 
       this.socket.emit('computerHitLeftWall', data)
-      //return false
+      if (this.state.computer === 1) return false
     }
 
-    //if (head.x === )
-
     if (head.x === this.numCells) {
-      console.log("right wall hit")
       let data = {yaxis: this.state.snake[0].y, snakeLength: this.state.snake.length }
       this.socket.emit('computerHitRightWall', data)
-      //return false
+      
+      if (this.state.computer === 2) return false
     }
 
     if (head.y === -1 || head.y === this.numCells) {
-      //return false
+      return false
     } 
 
     return true
   }
 
   endGame() {
-    alert("lol")
+    alert("Well done! You got " + this.state.score + " points!")
     this.startGame()
   }
 
@@ -138,32 +115,21 @@ class App extends Component {
   }
 
   resumeGame(yaxis, length) {
-     console.log("starting as" + length)
-
     this.removeTimers()
 
     let newSnake = [{x: this.state.computer === 1? this.numCells -1 : 0, y: yaxis}]
 
-    //this.state.computer === 1? (this.numCells -1 + i) : (0 - i)
-
     for (let i = 1; i < length; i++) {
-      console.log('PUSHING')
       newSnake.push({x: this.state.computer === 1? (this.numCells -1 + i) : (0 - i), y: yaxis})
     }
 
-    this.setState({snake: newSnake})
-
-    //console.log(newSnake)
-
     this.setState({
-      
+      snake: newSnake,
       dy: 0,
       dx: this.state.computer === 1? -1  : 1,
       food: [10, 10]
     })
-
     this.moveSnakeInterval = setInterval(this.moveSnake, 400)
-
     this.el.focus()
   }
 
@@ -178,31 +144,23 @@ class App extends Component {
 
    componentDidMount() {
     this.removeTimers();
-    this.socket = openSocket('http://localhost:8000');
+
+    this.socket = openSocket('http://192.168.0.12:8000');
+
     this.socket.on('youAreComputer', computer => {
       this.setState({computer})
       console.log("this is computer: " + computer)
     });
 
     this.socket.on('updateNewScore', score => {
-      //this.incrementScore(score)
       this.setState({score: score})
-      
     })
 
     this.socket.on('startGameplay', (data) => {
-
       this.setState({currentGameplayComputer: data.currentGameplayComputer})
-
       if (this.state.currentGameplayComputer === this.state.computer) {
-        //console.log("OK ITS THIS COMPUTER")
-
         this.resumeGame(data.yaxis, data.length)
-      } else {
-        //this.setState({snake: []})
-        //this.reduceSnakeInterval = setInterval(this.reduceSnake, 130)
-        //this.pauseGame()
-      }
+      } 
     })
 
     this.startGame()
@@ -215,27 +173,30 @@ class App extends Component {
     const DOWN_KEY = 40;
 
     if (this.state.changingDirection) return
+    
     this.setState({changingDirection: true})
 
 
-      const keyPressed = keyCode;
-      //const goingUp = dy === -10;
-      const goingUp = this.state.dy === -1;
-     const goingDown = this.state.dy === 1;
-      const goingRight = this.state.dx === 1;
-      const goingLeft = this.state.dx === -1;
-      if (keyPressed === LEFT_KEY && !goingRight) {
-        this.setState({dx: -1, dy: 0})
-      }
-      if (keyPressed === UP_KEY && !goingDown) {
-        this.setState({dx: 0, dy: -1})
-      }
-      if (keyPressed === RIGHT_KEY && !goingLeft) {
-        this.setState({dx: 1, dy: 0})
-      }
-      if (keyPressed === DOWN_KEY && !goingUp) {
-        this.setState({dx: 0, dy: 1})
-      }
+    const keyPressed = keyCode;
+    const goingUp = this.state.dy === -1;
+    const goingDown = this.state.dy === 1;
+    const goingRight = this.state.dx === 1;
+    const goingLeft = this.state.dx === -1;
+    if (keyPressed === LEFT_KEY && !goingRight) {
+      this.setState({dx: -1, dy: 0})
+    }
+    
+    if (keyPressed === UP_KEY && !goingDown) {
+      this.setState({dx: 0, dy: -1})
+    }
+    
+    if (keyPressed === RIGHT_KEY && !goingLeft) {
+      this.setState({dx: 1, dy: 0})
+    }
+    
+    if (keyPressed === DOWN_KEY && !goingUp) {
+      this.setState({dx: 0, dy: 1})
+    }
   }
 
   componentWillUnmount() {
@@ -243,15 +204,6 @@ class App extends Component {
   }
 
   render() {
-
-
-    
-
-    
-
-
-
-
     this.numCells = Math.floor(350 / 15);
     const cellSize = 350 / this.numCells;
     const cellIndexes = Array.from(Array(this.numCells).keys());
@@ -259,17 +211,12 @@ class App extends Component {
       return cellIndexes.map(x => {
         let isCellSnake = this.state.snake.filter(i => i.x === x && i.y === y).length === 1 ? true : false
         const isCellFood = this.state.food[0] === x  && this.state.food[1] === y
-
-
-
         return (
 
           <GridCell key={x} size={cellSize} snake={isCellSnake} food={isCellFood} />
         );
       });
     });
-
-
 
     return (
       <div className="App" >
@@ -294,7 +241,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
